31359d1a1737d4cae7e75f31d0f7bf1ef7c3d80d
Oskar Almå <os6164al-s@student.lu.se>
2023-09-23 19:31:41 +0200

    Initial Commit

---
+++ beta2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  beta2
+//
+//  Created by Oskar Almå on 2023-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ beta2/beta2App.swift
@@ -0,0 +1,17 @@
+//
+//  beta2App.swift
+//  beta2
+//
+//  Created by Oskar Almå on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct beta2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
