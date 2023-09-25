8902a38668d2081c23616fe90e70012ace640230
Oskar Almå <os6164al-s@student.lu.se>
2023-09-25 21:52:31 +0200

    21.52

--- beta2/FrontEnd/DailyView.swift
+++ beta2/FrontEnd/DailyView.swift
@@ -7,7 +7,60 @@
 
 import SwiftUI
 
-import SwiftUI
+struct ChallengeTileView: View {
+    let challenge: Challenge
+    let category: ChallengeCategory
+    @State private var isTextVisible = false
+
+    var body: some View {
+        if let evidence = challenge.evidence, challenge.isCompleted {
+            VStack(spacing: 0) {
+                Image(uiImage: evidence)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.primary, lineWidth: 2))
+                    .onTapGesture {
+                        withAnimation {
+                            isTextVisible.toggle()
+                            generateHapticFeedback()
+                        }
+                    }
+                if isTextVisible {
+                    Text(challenge.title)
+                        .font(.custom("Avenir", size: 20))
+                        .foregroundColor(Color.primary)
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: UIScreen.main.bounds.width)
+                        .padding(.horizontal)
+                }
+                else {
+                    Rectangle()
+                        .fill(Color(red: (28+95)/255, green: (77+95)/255, blue: (42+95)/255))
+                        .frame(width: 5, height: 20)
+                }
+            }
+        } else {
+            VStack(spacing: 0){
+                ZStack {
+                    Circle()
+                        .fill(Color(red: (28+95)/255, green: (77+95)/255, blue: (42+95)/255))
+                        .frame(width: 150, height: 150)
+                    Text("NEW CHALLENGE!")
+                        .font(.custom("Avenir", size: 20))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 150)
+                        .padding(.horizontal)
+                }
+                Rectangle()
+                    .fill(Color(red: 240/255, green: 240/255, blue: 240/255))
+                    .frame(width: 5, height: 20)
+            }
+        }
+    }
+}
 
 struct DailyChallengeView: View {
     @ObservedObject var challengeData: ChallengeData
@@ -25,7 +78,7 @@ struct DailyChallengeView: View {
                     .frame(maxWidth: .infinity)
                     .padding(.horizontal)
                 
-                if let dailyChallenge = challengeData.dailyChallenge {
+                if let dailyChallenge = $challengeData.dailyChallenge {
                     ChallengeTileView(challenge: dailyChallenge, category: .daily)
                 } else {
                     Text("No daily challenge available.")
