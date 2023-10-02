9ea7ea6d5a9f1d278db67a4d4991380aa1bc463f
Oskar Almå <os6164al-s@student.lu.se>
2023-10-02 21:53:27 +0200

    nytt igen såklart

--- beta2/Firestore/FirebaseManager.swift
+++ beta2/Firestore/FirebaseManager.swift
@@ -11,6 +11,7 @@ import FirebaseFirestore
 import FirebaseStorage
 
 struct Evidence {
+    var userId: String
     var comment: String
     var imageUrl: String
     // Add other fields as needed
@@ -32,7 +33,7 @@ class FirebaseManager {
     ///   - imuage: The image to upload.
     ///   - comment: Comment associated with the evidence.
     ///   - challengeId: The ID of the challenge to associate the evidence with.
-    func uploadEvidence(image: UIImage, comment: String, challengeId: Int, completion: @escaping (Result<String, Error>) -> Void) {
+    func uploadEvidence(for userId: String, image: UIImage, comment: String, challengeId: Int, completion: @escaping (Result<String, Error>) -> Void) {
         // 1. Upload image to Firebase Storage
         let storageRef = storage.reference().child("evidence/\(challengeId).jpg")
         
@@ -57,7 +58,7 @@ class FirebaseManager {
                     
                     if let downloadURL = url?.absoluteString {
                         // 3. Once upload is successful and download URL is retrieved, update Firestore with evidence details
-                        self.updateFirestoreWithEvidence(challengeId: challengeId, comment: comment, imageUrl: downloadURL) { result in
+                        self.updateFirestoreWithEvidence(userId: userId, challengeId: challengeId, comment: comment, imageUrl: downloadURL) { result in
                             switch result {
                             case .success:
                                 completion(.success(downloadURL))
@@ -71,12 +72,13 @@ class FirebaseManager {
         }
     }
     
-    private func updateFirestoreWithEvidence(challengeId: Int, comment: String, imageUrl: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    private func updateFirestoreWithEvidence(userId: Int, challengeId: Int, comment: String, imageUrl: String, completion: @escaping (Result<Void, Error>) -> Void) {
         let evidenceDocRef = db.collection("evidence").document("\(challengeId)")
         
         let evidenceData: [String: Any] = [
-            "comment": comment,
-            "imageUrl": imageUrl
+                   "userId": userId,
+                   "comment": comment,
+                   "imageUrl": imageUrl
             // Add other evidence-related fields as needed
         ]
         
@@ -92,23 +94,20 @@ class FirebaseManager {
         }
     }
     
-    func fetchEvidence(for challengeId: Int, completion: @escaping (Result<Evidence, Error>) -> Void) {
-        let evidenceDocRef = db.collection("evidence").document("\(challengeId)")
-        
-        evidenceDocRef.getDocument { (document, error) in
-            if let error = error {
-                print("Error fetching evidence: \(error.localizedDescription)")
-                completion(.failure(error))
-                return
-            }
+    func fetchEvidence(for userId: String, challengeId: Int, completion: @escaping (Result<Evidence, Error>) -> Void) {
+            let evidenceDocRef = db.collection("evidence").document("\(userId)_\(challengeId)")
             
-            if let document = document, document.exists, let data = document.data() {
-                let comment = data["comment"] as? String ?? ""
-                let imageUrl = data["imageUrl"] as? String ?? ""
+            evidenceDocRef.getDocument { (document, error) in
+                // ... (rest of the code remains unchanged)
                 
-                let evidence = Evidence(comment: comment, imageUrl: imageUrl)
-                completion(.success(evidence))
-            } else {
+                if let document = document, document.exists, let data = document.data() {
+                    let userId = data["userId"] as? String ?? ""
+                    let comment = data["comment"] as? String ?? ""
+                    let imageUrl = data["imageUrl"] as? String ?? ""
+                    
+                    let evidence = Evidence(userId: userId, comment: comment, imageUrl: imageUrl)
+                    completion(.success(evidence))
+                } else {
                 print("Document does not exist")
                 completion(.failure(NSError(domain: "com.yourapp", code: -1, userInfo: [NSLocalizedDescriptionKey: "Document does not exist"])))
             }
