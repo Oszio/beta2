14221015f5a0665eadda28db94ab3ab4b89cc2bb
Oskar Almå <os6164al-s@student.lu.se>
2023-10-12 14:04:26 +0200

    SNART SÅ

---
+++ beta2/FrontEnd/Challenges/ChallengeManager.swift
@@ -0,0 +1,8 @@
+//
+//  ChallangeManager.swift
+//  beta2
+//
+//  Created by Oskar Almå on 2023-10-12.
+//
+
+import Foundation
---
+++ beta2/FrontEnd/Challenges/ChallengeModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChallengeModel.swift
+//  beta2
+//
+//  Created by Oskar Almå on 2023-10-12.
+//
+
+import Foundation
