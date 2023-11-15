a83bdfb4d0dd7a40fba5d7d22a18ec993d294f69
Oskar Almå <os6164al-s@student.lu.se>
2023-11-15 23:44:34 +0100

    jul igen

---
+++ beta2/FrontEnd/Challenges/AnimatedBackground.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedBackground.swift
+//  beta2
+//
+//  Created by Oskar Almå on 2023-11-15.
+//
+
+import SwiftUI
+
+struct AnimatedBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedBackground()
+}
